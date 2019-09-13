7d4009bbe2a8492ac77526bf9e132c79df2cab3d
suren <sur.muradyan1@gmail.com>
2019-09-13 13:08:10 +0400

    Changes

--- resources/views/who_are_you.blade.php
+++ resources/views/who_are_you.blade.php
@@ -8,32 +8,31 @@
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container">
+        <a class="navbar-brand" href="#" id="logo">
+            <img src="{{asset('svg/foxy_face_winkle.svg')}}" alt="foxy_face_winkle">
+            <img src="{{asset('svg/Component_1.svg')}}" alt="foxy">
+        </a>
+
+        <a id="models" href="#" class="pull-left">Models</a>
+        <a id="videos" href="#" class="">Videos</a>
+
+        <div id="search_button" class="d-flex justify-content-center">
+            <img src="{{asset('svg/search_icon.svg')}}">
+        </div>
+        <div id="basket_button" class="d-flex justify-content-center align-items-center">
             <div>
-                <a class="navbar-brand" href="#" id="logo">
-                    <img src="{{asset('svg/foxy_face_winkle.svg')}}" alt="foxy_face_winkle">
-                    <img src="{{asset('svg/Component_1.svg')}}" alt="foxy">
-                </a>
-                <button id="models" type="button" class="btn btn-light pull-left">Models</button>
-                <button id="videos" type="button" class="btn btn-light">Videos</button>
-            </div>
-            <div class="d-flex justify-content-center flex-row" id="navbar_items_right">
-                <div id="search_button" class="d-flex justify-content-center">
-                    <img src="{{asset('svg/search_icon.svg')}}">
-                </div>
-                <div id="basket_button" class="d-flex justify-content-center">
-                    <img src="{{asset('svg/basket_icon.svg')}}">
-                </div>
-                <button id="sign_up" type="button" class="btn">Sign Up</button>
-                <button id="login" type="button" class="btn">Login</button>
+                <p class="d-flex justify-content-center align-items-center">3</p>
+                <img src="{{asset('svg/CartIcon_Group.svg')}}" width="20" height="20">
             </div>
         </div>
+        <button id="sign_up" type="button" class="btn">Sign Up</button>
+        <button id="login" type="button" class="btn">Login</button>
     </nav>
-    <div class="d-flex align-items-center flex-column">
+    <div class="d-flex flex-column" id="header_who_are_you">
         <h1>Who are you?</h1>
         <div id="header_under_liner"></div>
     </div>
-    <div id="main_content" class="d-flex justify-content-center flex-row align-items-center">
+    <div id="main_content" class="d-flex justify-content-center flex-row">
         <div id="fan_window" class="d-flex align-items-center flex-column">
             <h2>Fan</h2>
             <div class="align-items-left middle_list">
@@ -75,10 +74,10 @@
                     </div>
                 </div>
             </div>
-            <button id="sign_up_for_free" type="button" class="btn btn-light bottom_button">
+            <div id="sign_up_for_free" class="d-flex justify-content-center flex-row align-items-center bottom_button">
                 SIGN UP FOR FREE
                 <img src="{{asset('svg/heart.svg')}}">
-            </button>
+            </div>
         </div>
         <div id="or_window" class="d-flex align-items-center flex-column">
             <img src="{{asset('svg/Group_2173.svg')}}">
@@ -125,10 +124,14 @@
                     </div>
                 </div>
             </div>
-            <button id="start_my_store" type="button" class="btn btn-light bottom_button">
+            <div id="start_my_store" class="d-flex justify-content-center flex-row align-items-center bottom_button">
                 START MY STORE
-                <img src="{{asset('svg/heart.svg')}}">
-            </button>
+                <img src="{{asset('svg/store-clipart-transparent-780888-5417819_white.svg')}}">
+            </div>
+{{--            <button id="start_my_store" type="button" class="btn btn-light bottom_button">--}}
+{{--                START MY STORE--}}
+{{--                <img src="{{asset('svg/store-clipart-transparent-780888-5417819_white.svg')}}">--}}
+{{--            </button>--}}
         </div>
     </div>
     <div id="login_now" class="d-flex justify-content-center">
