0b8b91ce533c3531ad761f5f2d5cdbd557f3b658
Zhenya Kirakosyan <zhenya.kirakosyan93@gmail.com>
2019-09-13 18:27:24 +0400

    Improvment

--- resources/views/__who_are_you.blade.php
+++ resources/views/__who_are_you.blade.php
@@ -7,39 +7,33 @@
     <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}" >
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container">
-        <div>
-            <a class="navbar-brand" href="#" id="logo">
-                <img src="{{asset('svg/foxy_face_winkle.svg')}}" alt="foxy_face_winkle">
-                <img src="{{asset('svg/Component_1.svg')}}" alt="foxy">
-            </a>
-            <button id="models" type="button" class="btn btn-light pull-left">Models</button>
-            <button id="videos" type="button" class="btn btn-light">Videos</button>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#" id="logo">
+            <img src="{{asset('svg/foxy_face_winkle.svg')}}" alt="foxy_face_winkle">
+            <img src="{{asset('svg/Component_1.svg')}}" alt="foxy">
+        </a>
+
+        <a id="models" href="#" class="pull-left">Models</a>
+        <a id="videos" href="#" class="">Videos</a>
+
+        <div id="search_button" class="d-flex justify-content-center">
+            <img src="{{asset('svg/search_icon.svg')}}">
         </div>
-        <div class="d-flex justify-content-center flex-row" id="navbar_items_right">
-            <div id="search_button" class="d-flex justify-content-center">
-                <img src="{{asset('svg/search_icon.svg')}}">
+        <div id="basket_button" class="d-flex justify-content-center align-items-center">
+            <div>
+                <p class="d-flex justify-content-center align-items-center">3</p>
+                <img src="{{asset('svg/CartIcon_Group.svg')}}" width="20" height="20">
             </div>
-            <div id="basket_button" class="d-flex justify-content-center">
-                <img src="{{asset('svg/basket_icon.svg')}}">
-            </div>
-            <button id="sign_up" type="button" class="btn">Sign Up</button>
-            <button id="login" type="button" class="btn">Login</button>
         </div>
-    </div>
-</nav>
-<div id="header_who_are_you">
-    <div class="d-flex align-items-center flex-column">
+        <button id="sign_up" type="button" class="btn">Sign Up</button>
+        <button id="login" type="button" class="btn">Login</button>
+    </nav>
+    <div class="d-flex flex-column" id="header_who_are_you">
         <h1>Who are you?</h1>
         <div id="header_under_liner"></div>
     </div>
-</div>
-<div id="main_content" class="d-flex justify-content-center flex-row align-items-center">
-    <div class="row">
-        <div class="col-xs-12 col-sm-5">
-            <div class="d-flex justify-content-center">
-        <div id="fan_window" class="d-flex align-items-center flex-column pull-right">
+    <div id="main_content" class="d-flex justify-content-center flex-row">
+        <div id="fan_window" class="d-flex align-items-center flex-column">
             <h2>Fan</h2>
             <div class="align-items-left middle_list">
                 <h4>Signup for a free account to:</h4>
@@ -85,18 +79,10 @@
                 <img src="{{asset('svg/heart.svg')}}">
             </div>
         </div>
-            </div>
+        <div id="or_window" class="d-flex align-items-center flex-column">
+            <img src="{{asset('svg/Group_2173.svg')}}">
+            <p>or</p>
         </div>
-        <div class="col-xs-12 col-sm-2">
-            <div class="d-flex justify-content-center align-items-center" style="height: 100%">
-                <div id="or_window" class="d-flex align-items-center flex-column">
-                    <img src="{{asset('svg/Group_2173.svg')}}">
-                    <p>or</p>
-                </div>
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-5">
-            <div class="d-flex justify-content-center">
         <div id="model_window" class="d-flex align-items-center flex-column">
             <h2>Model</h2>
             <div class="align-items-left middle_list">
@@ -144,15 +130,12 @@
             </div>
 {{--            <button id="start_my_store" type="button" class="btn btn-light bottom_button">--}}
 {{--                START MY STORE--}}
-{{--                <img src="{{asset('svg/heart.svg')}}">--}}
+{{--                <img src="{{asset('svg/store-clipart-transparent-780888-5417819_white.svg')}}">--}}
 {{--            </button>--}}
         </div>
-            </div>
-        </div>
     </div>
-</div>
-<div id="login_now" class="d-flex justify-content-center">
-    Already have an account?&nbsp; <a href="#"> Login now →</a>
-</div>
+    <div id="login_now" class="d-flex justify-content-center">
+        Already have an account?&nbsp; <a href="#"> Login now →</a>
+    </div>
 </body>
 </html>
