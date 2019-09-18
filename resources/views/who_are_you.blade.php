46310db3cd6b781850d0e3c35bc9473394bdf39c
Zhenya Kirakosyan <zhenya.kirakosyan93@gmail.com>
2019-09-18 15:06:38 +0400

    Improvment

--- resources/views/who_are_you.blade.php
+++ resources/views/who_are_you.blade.php
@@ -8,7 +8,7 @@
     <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
 </head>
 <body>
-<nav class="navbar navbar-expand-md navbar-light bg-light">
+<nav class="navbar navbar-expand-md navbar-light bg-light fixed-top">
     <div class="container">
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -70,11 +70,8 @@
             </div>
         </div>
     </div>
-{{--    <div class="row">--}}
-        <div class="d-none d-md-block">
-            <div class="row">
-            <div class="d-flex justify-content-center flex-row" id="main_content_2">
-{{--        <div class="d-flex justify-content-center">--}}
+    <div class="row">
+        <div class="d-flex justify-content-center flex-row" id="main_content_2">
             <div id="fan_window" class="d-flex align-items-center flex-column">
                 <h2>Fan</h2>
                 <div class="align-items-left middle_list">
@@ -123,14 +120,12 @@
                     </div>
                 </div>
             </div>
-{{--        </div>--}}
-        <div id="or_window" class="d-flex align-items-center">
+            <div id="or_window" class="d-flex align-items-center">
             <div class="d-flex justify-content-center align-items-center flex-column">
                 <img src="{{asset('svg/Group_2173.svg')}}">
                 <p>or</p>
             </div>
         </div>
-{{--        <div class="d-flex justify-content-center">--}}
             <div id="model_window" class="d-flex align-items-center flex-column">
                 <h2>Model</h2>
                 <div class="align-items-left middle_list">
@@ -180,125 +175,7 @@
                 </div>
             </div>
         </div>
-            </div>
-        </div>
-        <div class="d-block d-md-none" id="main_content_3">
-            <div class="col-sm-12">
-                <div class="d-flex justify-content-center">
-                <div id="fan_window" class="d-flex align-items-center flex-column">
-                    <h2>Fan</h2>
-                    <div class="align-items-left middle_list">
-                        <h4>Signup for a free account to:</h4>
-                        <div class="media">
-                            <img class="align-self-start mr-2" src="{{asset('svg/Group_3414.svg')}}">
-                            <div class="media-body">
-                                <p class="d-flex align-items-center">Chat with your dream girls</p>
-                            </div>
-                        </div>
-                        <div class="media">
-                            <img class="align-self-start mr-2" src="{{asset('svg/Group_3414.svg')}}">
-                            <div class="media-body">
-                                <p class="d-flex align-items-center">Order Custom Videos</p>
-                            </div>
-                        </div>
-                        <div class="media">
-                            <img class="align-self-start mr-2" src="{{asset('svg/Group_3414.svg')}}">
-                            <div class="media-body">
-                                <p class="d-flex align-items-center">Access private Snapchats</p>
-                            </div>
-                        </div>
-                        <div class="media">
-                            <img class="align-self-start mr-2" src="{{asset('svg/Group_3414.svg')}}">
-                            <div class="media-body">
-                                <p class="d-flex align-items-center">Buy exclusive Videos</p>
-                            </div>
-                        </div>
-                        <div class="media">
-                            <img class="align-self-start mr-2" src="{{asset('svg/Group_3414.svg')}}">
-                            <div class="media-body">
-                                <p class="d-flex align-items-center">Find your new crush</p>
-                            </div>
-                        </div>
-                        <div class="media">
-                            <img class="align-self-start mr-2" src="{{asset('svg/Group_3414.svg')}}">
-                            <div class="media-body">
-                                <p class="d-flex align-items-center">And much more…</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div id="sign_up_for_free" class="d-flex justify-content-center bottom_button">
-                        <div class="d-flex justify-content-center flex-row align-items-center">
-                            <span>SIGN UP FOR FREE</span>
-                            <img src="{{asset('svg/heart.svg')}}">
-                        </div>
-                    </div>
-                </div>
-                </div>
-            </div>
-            <div class="col-sm-12">
-                <div class="d-flex justify-content-center">
-            <div id="or_window" class="d-flex align-items-center">
-                <div class="d-flex justify-content-center align-items-center flex-column">
-                    <img src="{{asset('svg/Group_2173.svg')}}">
-                    <p>or</p>
-                </div>
-            </div>
-                </div>
-            </div>
-            <div class="col-sm-12">
-                <div class="d-flex justify-content-center">
-                    <div id="model_window" class="d-flex align-items-center flex-column">
-                    <h2>Model</h2>
-                    <div class="align-items-left middle_list">
-                        <h4>Open your own store to:</h4>
-                        <div class="media">
-                            <img class="align-self-start mr-2" src="{{asset('svg/Group_3413.svg')}}">
-                            <div class="media-body">
-                                <p class="d-flex align-items-center">Sell videos&photosets</p>
-                            </div>
-                        </div>
-                        <div class="media">
-                            <img class="align-self-start mr-2" src="{{asset('svg/Group_3413.svg')}}">
-                            <div class="media-body">
-                                <p class="d-flex align-items-center">Sell customs</p>
-                            </div>
-                        </div>
-                        <div class="media">
-                            <img class="align-self-start mr-2" src="{{asset('svg/Group_3413.svg')}}">
-                            <div class="media-body">
-                                <p class="d-flex align-items-center">Sell 1-on-1 video chats</p>
-                            </div>
-                        </div>
-                        <div class="media">
-                            <img class="align-self-start mr-2" src="{{asset('svg/Group_3413.svg')}}">
-                            <div class="media-body">
-                                <p class="d-flex align-items-center">Sell custom subscriptions</p>
-                            </div>
-                        </div>
-                        <div class="media">
-                            <img class="align-self-start mr-2" src="{{asset('svg/Group_3413.svg')}}">
-                            <div class="media-body">
-                                <p class="d-flex align-items-center">Use your own custom domain</p>
-                            </div>
-                        </div>
-                        <div class="media">
-                            <img class="align-self-start mr-2" src="{{asset('svg/Group_3413.svg')}}">
-                            <div class="media-body">
-                                <p class="d-flex align-items-center">And much more…</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div id="start_my_store" class="d-flex justify-content-center bottom_button">
-                        <div class="d-flex justify-content-center flex-row align-items-center">
-                            <span>START MY STORE</span>
-                            <img src="{{asset('svg/store-clipart-transparent-780888-5417819_white.svg')}}">
-                        </div>
-                    </div>
-                </div>
-                </div>
-            </div>
-        </div>
-{{--    </div>--}}
+    </div>
     <div id="login_now" class="d-flex justify-content-center">
         Already have an account?&nbsp; <a href="#"> Login now →</a>
     </div>
